33638624384980ce1dc56c75a80fa353f49996e1
JonathanDussot <jonathandussot@gmail.com>
2024-07-12 15:32:53 +0000

    readies code for deploy

--- gems/settings.py
+++ gems/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(e37z4sjf%(jth0f%(-puqufvots^gp2_%$x601l**q!eydpf4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jonathandus-hiddengembl-11uciaduujz.ws-eu115.gitpod.io']
+ALLOWED_HOSTS = ['8000-jonathandus-hiddengembl-11uciaduujz.ws-eu115.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
