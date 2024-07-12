6134095a2dd587c957ce87def2caebc2c7728ae1
JonathanDussot <jonathandussot@gmail.com>
2024-07-12 15:57:44 +0000

    Add secret key

--- gems/settings.py
+++ gems/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(e37z4sjf%(jth0f%(-puqufvots^gp2_%$x601l**q!eydpf4'
+SECRET_KEY = 'rochnwnd98dnnd3090u"%/$fff'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
