c920793313c774f26b75b0d4d9357b8898757a79
JonathanDussot <jonathandussot@gmail.com>
2024-07-18 21:37:28 +0000

    Change URL to connect to external Database

--- gems/settings.py
+++ gems/settings.py
@@ -98,17 +98,17 @@ WSGI_APPLICATION = 'gems.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.gitpod.io",
