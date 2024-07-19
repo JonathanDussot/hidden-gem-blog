4c249f6917b1b7c1d413c1ec075e0431e512f879
JonathanDussot <jonathandussot@gmail.com>
2024-07-19 08:37:59 +0000

    Add resources app to project.

---
+++ resources/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import Resources
+
+# Create your views here.
+
+
+def travel_resources(request):
+    """
+    Renders the resources page
+    """
+    resources = Resources.objects.all().order_by('-updated_on').first()
+
+    return render(
+        request,
+        "resources/resources.html",
+        {"resources": resources},
+    )
