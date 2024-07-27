135c5b1cdb43ae231dd5a04a672d713c93ffe3a9
Jonathan Dussot <jonathandussot@gmail.com>
2024-07-27 13:17:10 +0000

    Add comments to apps.py and admin.py in about app.

--- about/admin.py
+++ about/admin.py
@@ -3,13 +3,22 @@ from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
+# Register the About model with the admin site and enables summernote for
+# content.
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
+    """
+    Admin configuration for the About model.
+    Uses Summernote for rich text editing in the content field.
+    """
     summernote_fields = ('content',)
 
 
+# Register the CollaborateRequest model with the admin site
 @admin.register(CollaborateRequest)
 class CollaborateRequestAdmin(admin.ModelAdmin):
-
+    """
+    Admin configuration for the CollaborateRequest model.
+    Displays 'message' and 'read' fields in the list view.
+    """
     list_display = ('message', 'read',)
--- about/apps.py
+++ about/apps.py
@@ -2,5 +2,14 @@ from django.apps import AppConfig
 
 
 class AboutConfig(AppConfig):
+    """
+    This class configures the 'about' application.
+
+    Attributes:
+        default_auto_field (str): Specifies the type of field to use for
+        auto-generated primary keys. 'BigAutoField' is used to accommodate
+        larger integers. name (str): The name of the application. This is
+        used by Django to refer to the application.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'about'
