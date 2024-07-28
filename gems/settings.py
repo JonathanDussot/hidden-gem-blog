2d65f382165935e80d497e58cbc444b079e1158f
Jonathan Dussot <jonathandussot@gmail.com>
2024-07-28 10:26:04 +0000

    Add features left to implement, validator testing, responsive and C.R.U.D. testing to features section in README.md.

--- gems/settings.py
+++ gems/settings.py
@@ -12,7 +12,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-jonathandus-hiddengembl-49mgyq4449z.ws.codeinstitute-ide.net', '.herokuapp.com']
+ALLOWED_HOSTS = [
+    '8000-jonathandus-hiddengembl-49mgyq4449z.ws.codeinstitute-ide.net',
+    '.herokuapp.com'
+    ]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -98,16 +101,20 @@ CSRF_TRUSTED_ORIGINS = [
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        + 'UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        + 'MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        + 'CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.'
+        + 'NumericPasswordValidator',
     },
 ]
 
